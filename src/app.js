dc2efd6a57221ba83fb70531b48239b1e04a8c07
Asanka Basnayake <inbox2asanka@gmail.com>
2020-03-20 19:20:22 +0530

    webpack build issue fixed

--- src/app.js
+++ src/app.js
@@ -1,78 +1,65 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({ 
+    region: 'ap-south-1',
+    apiVersion: '2012-08-10'
+});
+exports.lambdaHandler = async (event, context) => {
+ 
+// Non-Async way
+// dynamodb.putItem({
+//             TableName: 'CUSTOMER_LIST',
+//             Item: {
+//                 'CUSTOMER_ID': { N: '043' },
+//                 'CUSTOMER_NAME': { S: 'Corona' }
+//             }
+//         }
+//         , function(err, data) {
+//             if (err) {
+//                 console.log("err", err);
+//                 // reject(err);
+//                 callback(null, {
+//                     statusCode: 200,
+//                     body: JSON.stringify(err),
+//                 })
+//             } else {
+//                 console.log("success", data);
+//                 // resolve(data);
+//                 callback(null, {
+//                     statusCode: 200,
+//                     body: JSON.stringify(data),
+//                 })
+//             }
+//         })
+        
+// Async way
+const promise = new Promise(function(resolve, reject){
 
-let response;
-import { s3stream, s3 } from "./kinesis_controller/WriteS3"
-import { db } from "./dynamoDB/ddb_createtable"
-import express from "express"
+    // setTimeout(function(){
+    //     console.log("resolvedx")
+    //     resolve({statusCode: 200, body: "resolvedx"})
+    // }, 200)
 
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-
-exports.lambdaHandler = function (event, context, callback) {
-    const s3Promise = s3.listBuckets().promise()
-
-    const putObjectPromise = new Promise((resolve, reject) => {
-        s3.putObject({
-            Bucket: "aws-sam-cli-managed-default-samclisourcebucket-11ckjuy8eoxq8",
-            Key: "sample.txt",
-            Body: "Hello, World!",
-        }, function (err, data) {
-            if (err) {
-                console.log(err)
-                reject(err)
-            }
-            else {
-                console.log("Successfully uploaded data to ");
-                resolve(data)
-            }
-
-        });
+    dynamodb.putItem({
+        TableName: 'CUSTOMER_LIST',
+        Item: {
+            'CUSTOMER_ID': { N: '036' },
+            'CUSTOMER_NAME': { S: 'vilo' }
+        }
+    }
+    , function(err, data) {
+        if (err) {
+            console.log("err", err);
+            reject({statusCode:200, body:JSON.stringify(err)});
+        } else {
+            console.log("success", data);
+            resolve({statusCode:200, body:JSON.stringify(data)});
+        }
     })
+})
 
-    const dbPromise = new Promise((resolve, reject) => {
-        var params = {
-            TableName: 'CUSTOMER_LIST',
-            Item: {
-              'CUSTOMER_ID' : {N: '002'},
-              'CUSTOMER_NAME' : {S: JSON.stringify({name:"asanka", age: 22})}
-            }
-          };
-          
-          // Call DynamoDB to add the item to the table
-          db.ddb.putItem(params, function(err, data) {
-            if (err) {
-              console.log("Error", err);
-              reject(err)
-            } else {
-              console.log("Success", data);
-              resolve(data)
-            }
-          })
-    })
-    
+return promise
+};
 
-    Promise.all([dbPromise]).then((data) => {
-        callback(null, {
-            "statusCode": "200",
-            "body": JSON.stringify({
-                message: data,
-                time: "123456789"
-            })
-        })
-    })
-
-
-}
 
 
 /**
@@ -88,4 +75,3 @@ exports.lambdaHandler = function (event, context, callback) {
  * node .gyp run
  * https://blog.usejournal.com/how-to-run-native-addon-modules-for-node-js-in-aws-lambda-aa74e1a9010
  */
-
