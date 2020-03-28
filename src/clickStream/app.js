69d2622fe91e29546f09cc341940e62e6b99f88c
Asanka Basnayake <inbox2asanka@gmail.com>
2020-03-28 22:58:01 +0530

    writeClick Stream

---
+++ src/clickStream/app.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({ 
+    region: 'ap-south-1',
+    apiVersion: '2012-08-10'
+});
+exports.clickStreamWriteHandler = (event,context, callback) => {
+    const  {body } = event
+    const { type, item, count, timeStamp, basketId} = JSON.parse(body)
+    console.log(type, item, count, timeStamp, basketId, body)
+    dynamodb.putItem({
+            TableName: 'BASKET_EVENTS',
+            Item: {
+                'BASKET_ID': { S: basketId },
+                'EVENT_TYPE': { S: type },
+                'ITEM': {S: item},
+                'COUNT': {N: count.toString()},
+                'TIME_STAMP':{N: timeStamp.toString()}
+            }
+        }
+        , function(err, data) {
+            if (err) {
+                console.log("err", err);
+                // reject(err);
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(err),
+                })
+            } else {
+                console.log("success", data);
+                // resolve(data);
+                callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(data),
+                })
+            }
+        })
+}
